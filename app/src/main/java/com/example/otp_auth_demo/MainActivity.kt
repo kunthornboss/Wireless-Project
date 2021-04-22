d6513d16f78cdb4ae5f57ca6172acf840dcf190f
Kunthorn Phongluelert <bosskunthorn@gmail.com>
2021-04-22 16:35:22 +0700

    added login page

--- app/src/main/java/com/example/otp_auth_demo/MainActivity.kt
+++ app/src/main/java/com/example/otp_auth_demo/MainActivity.kt
@@ -6,6 +6,8 @@ import android.widget.ArrayAdapter
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_main.*
+import android.view.View
+import android.widget.*
 
 class MainActivity : AppCompatActivity() {
 
@@ -13,22 +15,31 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        spinner.adapter = ArrayAdapter(
-            this,
-            android.R.layout.simple_spinner_dropdown_item,
-            CountryData.countryNames
-        )
+        // get reference to all views
+        var et_user_name = findViewById(R.id.et_user_name) as EditText
+        var et_password = findViewById(R.id.et_password) as EditText
+        var btn_reset = findViewById(R.id.btn_reset) as Button
+        var btn_submit = findViewById(R.id.btn_submit) as Button
 
-        button.setOnClickListener {
-            val code =
-                CountryData.countryAreaCodes[spinner.selectedItemPosition]
-            if (mobileNumber.text.count() == 10) {
-                val i = Intent(this, OTPValidation::class.java).apply {
-                    putExtra("mobileNumber", code+mobileNumber.text.toString())
-                }
+        btn_reset.setOnClickListener {
+            // clearing user_name and password edit text views on reset button click
+            et_user_name.setText("")
+            et_password.setText("")
+        }
+
+        // set on-click listener
+        btn_submit.setOnClickListener {
+            val user_name = et_user_name.text.toString();
+            val password = et_password.text.toString();
+            val keyusername = "teacher";
+            val keypassword = "111";
+            Toast.makeText(this@MainActivity, user_name, Toast.LENGTH_LONG).show()
+
+            if(user_name == keyusername && password == keypassword) {
+                val i = Intent(this, RequestOTP::class.java)
                 startActivity(i)
             } else {
-                Toast.makeText(this, "Enter 10 digits mobile number",Toast.LENGTH_LONG).show()
+                Toast.makeText(this,"Incorrect username or password", Toast.LENGTH_LONG).show()
             }
         }
     }
